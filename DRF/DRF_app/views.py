677c414b7effd36de046ef6b0b70f48ccf4b3a15
sanyog-tecblic <sanyogpatel.tecblci@gmail.com>
2023-07-05 12:07:18 +0530

    add

--- DRF/DRF_app/views.py
+++ DRF/DRF_app/views.py
@@ -1,4 +1,3 @@
-
 from .models import Task,User
 from rest_framework.response import Response
 from .serializers import UserSerializer,TaskSerializer
@@ -6,6 +5,8 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.decorators import api_view
 
 from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework_simplejwt.tokens import TokenError
 from DRF_app.utilities.utils import get_tokens_for_user
 # Create your views here.
 
@@ -14,44 +15,53 @@ from DRF_app.utilities.utils import get_tokens_for_user
 #task = Task.objects.all() it retrives all the instances of Task Model
 #many=true defines that there are multiple instances to serailized
 #serializer.data property retrieves the serialized data as JSON format.
-
-@api_view(['GET'])
-def GetAllTask(request):
-    task = Task.objects.all()
-    serializer = TaskSerializer(task, many=True)
-    return Response(serializer.data)    
-    
 @api_view(['GET'])
 def GetAllUser(request):
     user= User.objects.all()
     serializer = UserSerializer(user,many=True)
     return Response(serializer.data)
 
-#request.data shows data sent in request
+@api_view(['GET'])
+def GetTaskByUser(request):
+    tasks = Task.objects.filter(user=request.user)
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def GetDoneTasks(request):
+    status = Task.objects.filter(status=1)
+    if status.exists:
+        tasks= Task.objects.filter(user=request.user,status=1)
+        serializer =TaskSerializer(tasks,many=True)
+        return Response(serializer.data)
+    else:
+        return Response("No done tasks found", status=201)
+    
 @api_view(['POST'])
 def Create_Task(request):
     if request.method == 'POST':
-      serializer = TaskSerializer(data=request.data)
-      
-      if serializer.is_valid():
-          serializer.save()
-          return Response("Task added", status=201)
-      else:
-          return Response(serializer.errors, status=400)
+        request.data['user']=request.user.id
+        serializer = TaskSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response("Task added", status=201)
+        else:
+            return Response(serializer.errors, status=400)
 
 @api_view(['PUT'])
-def Update_Task(request, task_id):
+def Update_Task(request, task_id):  
     try:
         task = Task.objects.get(id=task_id)
     except Task.DoesNotExist:
         return Response("Task not found", status=404)
-
-    if request.method == 'PUT':
+    if request.method == 'PUT' and task.user == request.user:
         serializer = TaskSerializer(task, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response("Task updated", status=200)
         return Response(serializer.errors, status=400)
+    else:
+        return Response("YOU CAN NOT PERFORM ANY ACTION IN OTHER USER'S TASK")
     
 @api_view(['DELETE'])
 def Delete_Task(request, task_id):
@@ -60,9 +70,30 @@ def Delete_Task(request, task_id):
         except Task.DoesNotExist:
             return Response("Task not found", status=404)
 
-        if request.method == 'DELETE':
+        if request.method == 'DELETE' and task.user == request.user:
             task.delete()
             return Response("Task deleted", status=204)
+
+@api_view(['PUT'])
+def MarkAsDone(request, task_id):
+    try: 
+        task = Task.objects.get(id=task_id)
+        # task.user == request.data['user'] 
+        print(task)
+    except Task.DoesNotExist:
+        return Response("Task not found", status=404)
+    print(task.user.id)
+    print(request.user.id)
+    if request.method == 'PUT' and task.user == request.user:
+        task.status = 1
+        task.save()
+        
+        serializer = TaskSerializer(task)
+        message = {"message": "Task marked as done."}  
+        return Response({**serializer.data, **message})  
+    
+    message = {"message": "You cannot perform any action on another user's task."}
+    return Response(message, status=403)
 
 class LogIn(APIView):
     authentication_classes = []
@@ -76,9 +107,7 @@ class LogIn(APIView):
                 token = get_tokens_for_user(user)
                 return Response({'access_token': token})
             else:
-                # Invalid credentials
                 return Response({"message": "Invalid credentials"}, status=401)
         except User.DoesNotExist:
-            # Invalid credentials4
             return Response({"message": "Invalid Credentials"}, status=401)
 
